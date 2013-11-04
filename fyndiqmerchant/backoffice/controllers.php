bb8c62e513c91bb925ffe6043a638d3f43d3de60
Mattias Lindvall <mattias@fyndiq.se>
2013-11-04 10:17:28 +0100

    Moving logic and renaming function that checks for an existing api connection.

--- fyndiqmerchant/backoffice/controllers.php
+++ fyndiqmerchant/backoffice/controllers.php
@@ -4,11 +4,14 @@ class FyndiqMerchantBackofficeControllers {
     public static function main($module) {
         $output = null;
 
-        $output .= FyndiqMerchantBackofficeControllers::handle_authentication($module);
-        $output .= FyndiqMerchantBackofficeControllers::handle_disconnect($module);
-        $output .= FyndiqMerchantBackofficeControllers::handle_products($module);
+        if (self::api_connection_exists($module)) {
+            $output .= self::handle_disconnect($module);
+            $output .= self::handle_products($module);
+        } else {
+            $output .= self::handle_authentication($module);
+        }
 
-        if (!FyndiqMerchantBackofficeControllers::check_api_connection($module)) {
+        if (!self::api_connection_exists($module)) {
             $output .= FyndiqMerchantForms::render('authenticate', $module);
         } else {
             $output .= FyndiqMerchantForms::render('something', $module);
@@ -17,7 +20,7 @@ class FyndiqMerchantBackofficeControllers {
         return $output;
     }
 
-    private static function check_api_connection($module) {
+    private static function api_connection_exists($module) {
         return (
             Configuration::hasKey($module->config_name.'_username') &&
             Configuration::hasKey($module->config_name.'_api_token')
@@ -27,52 +30,49 @@ class FyndiqMerchantBackofficeControllers {
     private static function handle_authentication($module) {
         $output = '';
 
-        if (!FyndiqMerchantBackofficeControllers::check_api_connection($module)) {
+        # handle authenticate form submission
+        if (Tools::isSubmit('submit_authenticate')) {
+            $username = strval(Tools::getValue('username'));
+            $api_token = strval(Tools::getValue('api_token'));
 
-            # handle authenticate form submission
-            if (Tools::isSubmit('submit_authenticate')) {
-                $username = strval(Tools::getValue('username'));
-                $api_token = strval(Tools::getValue('api_token'));
+            # validate parameters
+            if (empty($username) || empty($api_token)) {
+                $output .= $module->displayError(
+                    $module->l('Please specify a Username and API token.'));
 
-                # validate parameters
-                if (empty($username) || empty($api_token)) {
+            # ready to perform authentication
+            } else {
+
+                # authenticate with Fyndiq API
+                $authenticated = false;
+                try {
+                    $result = FyndiqAPI::call($module->user_agent, $username, $api_token, array());
+                    $authenticated = true;
+                } catch (FyndiqAPIConnectionFailed $e) {
                     $output .= $module->displayError(
-                        $module->l('Please specify a Username and API token.'));
+                        $module->l('Network error, cannot connect to Fyndiq API.'));
+                } catch (FyndiqAPIDataInvalid $e) {
+                    $output .= $module->displayError(
+                        $module->l('Error processing data: '.$e.message));
+                } catch (FyndiqAPIAuthorizationFailed $e) {
+                    $output .= $module->displayError(
+                        $module->l('Incorrect Username or API token. Please double check your provided values.'));
+                }
 
-                # ready to perform authentication
+                # authentication successful
+                if ($authenticated) {
+
+                    # store values in configuration, to maintain a permanent connection
+                    Configuration::updateValue($module->config_name.'_username', $username);
+                    Configuration::updateValue($module->config_name.'_api_token', $api_token);
+
+                    # display success message
+                    $output .= $module->displayConfirmation(
+                        $module->l('You are now connected to your Fyndiq merchant account.'));
+
                 } else {
-
-                    # authenticate with Fyndiq API
-                    $authenticated = false;
-                    try {
-                        $result = FyndiqAPI::call($module->user_agent, $username, $api_token, array());
-                        $authenticated = true;
-                    } catch (FyndiqAPIConnectionFailed $e) {
-                        $output .= $module->displayError(
-                            $module->l('Network error, cannot connect to Fyndiq API.'));
-                    } catch (FyndiqAPIDataInvalid $e) {
-                        $output .= $module->displayError(
-                            $module->l('Error processing data: '.$e.message));
-                    } catch (FyndiqAPIAuthorizationFailed $e) {
-                        $output .= $module->displayError(
-                            $module->l('Incorrect Username or API token. Please double check your provided values.'));
-                    }
-
-                    # authentication successful
-                    if ($authenticated) {
-
-                        # store values in configuration, to maintain a permanent connection
-                        Configuration::updateValue($module->config_name.'_username', $username);
-                        Configuration::updateValue($module->config_name.'_api_token', $api_token);
-
-                        # display success message
-                        $output .= $module->displayConfirmation(
-                            $module->l('You are now connected to your Fyndiq merchant account.'));
-
-                    } else {
-                        Configuration::deleteByName($module->config_name.'_username');
-                        Configuration::deleteByName($module->config_name.'_api_token');
-                    }
+                    Configuration::deleteByName($module->config_name.'_username');
+                    Configuration::deleteByName($module->config_name.'_api_token');
                 }
             }
         }
@@ -83,16 +83,14 @@ class FyndiqMerchantBackofficeControllers {
     private static function handle_disconnect($module) {
         $output = '';
 
-        if (FyndiqMerchantBackofficeControllers::check_api_connection($module)) {
-            if (Tools::isSubmit('submit_disconnect')) {
+        if (Tools::isSubmit('submit_disconnect')) {
 
-                # delete stored connection values
-                Configuration::deleteByName($module->config_name.'_username');
-                Configuration::deleteByName($module->config_name.'_api_token');
+            # delete stored connection values
+            Configuration::deleteByName($module->config_name.'_username');
+            Configuration::deleteByName($module->config_name.'_api_token');
 
-                $output .= $module->displayConfirmation(
-                    $module->l('You have disconnected from your merchant account and Fyndiq API.'));
-            }
+            $output .= $module->displayConfirmation(
+                $module->l('You have disconnected from your merchant account and Fyndiq API.'));
         }
 
         return $output;
@@ -101,10 +99,8 @@ class FyndiqMerchantBackofficeControllers {
     private static function handle_products($module) {
         $output = '';
 
-        if (FyndiqMerchantBackofficeControllers::check_api_connection($module)) {
-            if (Tools::isSubmit('submit_saveproducts....')) {
-                ####
-            }
+        if (Tools::isSubmit('submit_saveproducts....')) {
+            ####
         }
 
         return $output;
