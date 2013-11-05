340bdb129635731da58c0944d1edfcb451541e01
Mattias Lindvall <mattias@fyndiq.se>
2013-11-05 13:04:41 +0100

    Fixing install/uninstall process
    Clearified logic in install/uninstall functions.
    Updated compatible version.
    Improved some user messages.

--- fyndiqmerchant/fyndiqmerchant.php
+++ fyndiqmerchant/fyndiqmerchant.php
@@ -21,7 +21,7 @@ class FyndiqMerchant extends Module {
         $this->version = '0.1';
         $this->author = 'Fyndiq AB';
         $this->need_instance = 0;
-        $this->ps_versions_compliancy = array('min' => '1.5.0', 'max' => '1.5.6');
+        $this->ps_versions_compliancy = array('min' => '1.5.0', 'max' => '1.5.7');
 
         # used as user agent string when calling the API
         $this->user_agent = $this->name.'-'.$this->version;
@@ -30,12 +30,12 @@ class FyndiqMerchant extends Module {
 
         $this->displayName = $this->l('Fyndiq');
         $this->description = $this->l('dfasdf');
-        $this->confirmUninstall = $this->l('adf');
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall this module?');
 
         if (!Configuration::get($this->config_name.'_username') ||
             !Configuration::get($this->config_name.'_api_token'))
         {
-            $this->warning = $this->l('Not authenticated with Fyndiq API.');
+            $this->warning = $this->l('You have not connected to your Fyndiq merchant account yet.');
         }
     }
 
@@ -43,22 +43,22 @@ class FyndiqMerchant extends Module {
         $ret = true;
 
         # do common module install
-        $ret &= parent::install();
+        $ret &= (bool)parent::install();
 
-        return $ret;
+        return (bool)$ret;
     }
 
     public function uninstall() {
         $ret = true;
 
         # do common module uninstall
-        $ret &= parent::uninstall();
+        $ret &= (bool)parent::uninstall();
 
         # do module specific uninstall
-        $ret &= Configuration::deleteByName($this->config_name.'_username');
-        $ret &= Configuration::deleteByName($this->config_name.'_api_token');
+        $ret &= (bool)Configuration::deleteByName($this->config_name.'_username');
+        $ret &= (bool)Configuration::deleteByName($this->config_name.'_api_token');
 
-        return $ret;
+        return (bool)$ret;
     }
 
     public function getContent() {
