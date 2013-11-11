de743dc120406d294a4cd1c51e19dab9f2cf964c
Mattias Lindvall <mattias@fyndiq.se>
2013-11-11 17:42:34 +0100

    Making fetching of products work equally in 1.4 and 1.5

--- fyndiqmerchant/backoffice/models/product.php
+++ fyndiqmerchant/backoffice/models/product.php
@@ -2,38 +2,43 @@
 
 class FmProduct {
     public static function get($product_id) {
+
+        $module = Module::getInstanceByName('fyndiqmerchant');
+
         $result = [];
 
-        $context = Context::getContext();
-        $product = new Product($product_id, false, $context->language->id);
+        $product = new Product($product_id, false, $module->language_id);
 
-        $image_types = ImageType::getImagesTypes();
-        foreach ($image_types as $type) {
-            if ($type['name'] == 'large_default') {
-                $image_type = $type;
-            }
-        }
-
-        // get all images for this product
-        $images = Image::getImages($context->language->id, $product_id);
-        // if product has any images
-        if (count($images) > 0) {
-            $image_link = $context->link->getImageLink($product->link_rewrite, $images[0]['id_image']);
-            //$context->link->getImageLink($product->link_rewrite, $image['id_image'], $image_type);
-        } else {
-            $image_link = false;
-        }
-        $result['image'] = $image_link;
-
+        $result['name'] = $product->name;
         $result['reference'] = $product->reference;
         $result['quantity'] = $product->quantity;
         $result['price'] = $product->price;
 
-        if (startswith(_PS_VERSION_, '1.4.')) {
-            $result['name'] = $product->name[1];
-        }
-        if (startswith(_PS_VERSION_, '1.5.')) {
-            $result['name'] = $product->name;
+        $image_types = ImageType::getImagesTypes();
+        $images = $product->getImages($module->language_id);
+
+        if (count($images) > 0) {
+            if (startswith(_PS_VERSION_, '1.4.')) {
+                foreach ($image_types as $type) {
+                    if ($type['name'] == 'medium') {
+                        $image_type = $type;
+                    }
+                }
+                $link = new Link();
+                $result['image'] = $link->getImageLink(
+                    $product->link_rewrite, $images[0]['id_image'], $image_type['name']);
+            }
+
+            if (startswith(_PS_VERSION_, '1.5.')) {
+                foreach ($image_types as $type) {
+                    if ($type['name'] == 'medium_default') {
+                        $image_type = $type;
+                    }
+                }
+                $context = Context::getContext();
+                $result['image'] = $context->link->getImageLink(
+                    $product->link_rewrite, $images[0]['id_image'], $image_type['name']);
+            }
         }
 
         return $result;
