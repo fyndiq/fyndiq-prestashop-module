03b680e05947939f2acb64b4163075e743dc835f
Mattias Lindvall <mattias@fyndiq.se>
2013-12-05 17:21:50 +0100

    Making the messages file more readable

--- fyndiqmerchant/messages.php
+++ fyndiqmerchant/messages.php
@@ -2,28 +2,29 @@
 
 class FmMessages {
     private static $messages = array(
-        'not-authenticated-warning' => 'You have not connected to your Fyndiq merchant account yet.',
-        'uninstall-confirm' => 'Are you sure you want to uninstall this module?',
-        'empty-username-token' => 'Please specify a Username and API token.',
-        'disconnect-confirm' => 'Are you sure you want to disconnect from your Fyndiq merchant account?',
-        'account-disconnected' => 'You have disconnected from your Fyndiq merchant account.',
-        'json-encode-fail' => 'Could not encode response json.',
+        'not-authenticated-warning'=>   'You have not connected to your Fyndiq merchant account yet.',
+        'uninstall-confirm'=>           'Are you sure you want to uninstall this module?',
+        'empty-username-token'=>        'Please specify a Username and API token.',
+        'disconnect-confirm'=>          'Are you sure you want to disconnect from your Fyndiq merchant account?',
+        'account-disconnected'=>        'You have disconnected from your Fyndiq merchant account.',
+        'json-encode-fail'=>            'Could not encode response json.',
 
-        'api-call-error' => 'Error when calling api',
-        'api-incorrect-credentials' => 'Incorrect Username or API token. Please double check your provided values.',
-        'api-invalid-data' => 'Error processing data',
-        'api-network-error' => 'Network error, cannot connect to Fyndiq API.',
-        'api-too-many-requests' => 'You have sent too many requests. Calm down!',
+        'api-call-error'=>              'Error when calling api',
 
+        'api-network-error'=>           'Network error, cannot connect to Fyndiq API',
+        'api-invalid-data'=>            'Error processing data',
+        'api-incorrect-credentials'=>   'Incorrect Username or API token. Please double check your provided values.',
+        'api-too-many-requests'=>       'You have sent too many requests. Calm down!',
+        'api-unknown-error'=>           'An unhandled error occurred when calling Fyndiq API. Sorry about this.',
 
-        'service-call-fail-head' => 'Connection failed',
-        'service-call-fail-message' => 'Could not connect to the module service.',
+        'service-call-fail-head'=>      'Connection failed',
+        'service-call-fail-message'=>   'Could not connect to the module service.',
 
-        'products-exported-title' => 'Products exported!',
-        'products-exported-message' => 'The products you selected have been exported to Fyndiq.',
+        'products-exported-title'=>     'Products exported!',
+        'products-exported-message'=>   'The products you selected have been exported to Fyndiq.',
 
-        'products-not-selected-title' => 'No products selected',
-        'products-not-selected-message' => 'You have to select at least one product to export.'
+        'products-not-selected-title'=>     'No products selected',
+        'products-not-selected-message'=>   'You have to select at least one product to export.'
     );
 
     public static function get($name) {
