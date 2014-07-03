7d4269f2468fe4e5f46d844a1103e46e301e595e
Mattias Lindvall <mattias@fyndiq.se>
2014-07-03 10:38:57 +0200

    Initial draft for a notification receiver
    Does not do anything, just debug prints.

---
+++ fyndiqmerchant/backoffice/notification_service.php
@@ -0,0 +1,29 @@
+<?php
+/*
+This file handles incoming requests from the automated notification system at Fyndiq.
+*/
+
+# import PrestaShop config, to enable use of PrestaShop classes, like Configuration
+$configPath = dirname(dirname(dirname(dirname($_SERVER['SCRIPT_FILENAME'])))).'/config/config.inc.php';
+if (file_exists($configPath)) {
+    require_once($configPath);
+} else {
+    exit;
+}
+
+require_once('../messages.php');
+require_once('./helpers.php');
+
+
+class FmNotificationService {
+    public static function main() {
+        pd($_GET);
+        pd($_POST);
+        $path_info = explode('/', $_SERVER['PATH_INFO']);
+        $shop_id = $path_info[1];
+        $passkey = $path_info[2];
+    }
+}
+
+
+FmNotificationService::main();
