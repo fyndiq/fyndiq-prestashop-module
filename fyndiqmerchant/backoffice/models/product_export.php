43bc485fcf1839179ab1433af67b75c4d2dfaf76
Håkan Nylén <hakan@dun.se>
2014-08-04 13:44:57 +0200

    add product ids to database
    fixed the query for adding a product to the database for the exporting.

--- fyndiqmerchant/backoffice/models/product_export.php
+++ fyndiqmerchant/backoffice/models/product_export.php
@@ -3,9 +3,9 @@
 class FmProductExport {
     public static function create($product_id, $fyndiq_id) {
         $module = Module::getInstanceByName('fyndiqmerchant');
-        $sql = 'insert into '._DB_PREFIX_.$module->config_name.'_products set
-            product_id = '.FmHelpers::db_escape($product_id).'
-            fyndiq_id = '.FmHelpers::db_escape($fyndiq_id).'
-        ';
+        $ret = (bool)Db::getInstance()->Execute('INSERT INTO ' . _DB_PREFIX_ . $module->config_name . '_products (product_id,fyndiq_id) VALUES (' . FmHelpers::db_escape(
+            $product_id
+        ) . ',' . FmHelpers::db_escape($fyndiq_id) . ')');
+        return $ret;
     }
 }
