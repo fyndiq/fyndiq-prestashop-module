cb4e72ee02629aafd7c45f1ea3234d08f6bf819e
Håkan Nylén <hakan@dun.se>
2014-08-08 12:50:39 +0200

    Moved this commit to correct branch
    Moved this commit to correct branch from branch order-get

--- fyndiqmerchant/backoffice/models/product_export.php
+++ fyndiqmerchant/backoffice/models/product_export.php
@@ -1,11 +1,47 @@
 <?php
 
 class FmProductExport {
+
+    /**
+     * install table to database
+     *
+     * @return bool
+     */
+    public static function install() {
+        $module = Module::getInstanceByName('fyndiqmerchant');
+        $ret = (bool)Db::getInstance()->Execute(
+            'create table if not exists '._DB_PREFIX_.$module->config_name.'_products (
+            id int(20) unsigned primary key,
+            product_id int(10) unsigned,
+            fyndiq_id int(20) unsigned)
+        ');
+        return $ret;
+    }
+
+    /**
+     * Create a row in table for mapping the products together.
+     *
+     * @param $product_id
+     * @param $fyndiq_id
+     */
     public static function create($product_id, $fyndiq_id) {
         $module = Module::getInstanceByName('fyndiqmerchant');
         $ret = (bool)Db::getInstance()->Execute('INSERT INTO ' . _DB_PREFIX_ . $module->config_name . '_products (product_id,fyndiq_id) VALUES (' . FmHelpers::db_escape(
             $product_id
         ) . ',' . FmHelpers::db_escape($fyndiq_id) . ')');
+        return $ret;
+    }
+
+    /**
+     * remove the table from database
+     *
+     * @return bool
+     */
+    public static function uninstall() {
+        $module = Module::getInstanceByName('fyndiqmerchant');
+        $ret = (bool)Db::getInstance()->Execute(
+            'drop table ' . _DB_PREFIX_ . $module->config_name . '_products'
+        );
         return $ret;
     }
 }
