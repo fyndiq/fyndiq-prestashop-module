d6cefde005080e1ec90125dcf02da593b47676a7
Håkan Nylén <hakan@dun.se>
2014-10-15 13:53:15 +0200

    added support for log exported quantity
    Added column for exported quantity to table.

--- fyndiqmerchant/backoffice/models/product_export.php
+++ fyndiqmerchant/backoffice/models/product_export.php
@@ -3,6 +3,20 @@
 class FmProductExport {
 
     /**
+     * Create a row in table for mapping the products together.
+     *
+     * @param $product_id
+     * @param $fyndiq_id
+     * @return bool
+     */
+    public static function create($product_id, $fyndiq_id) {
+        $module = Module::getInstanceByName('fyndiqmerchant');
+        $ret = (bool)Db::getInstance()->Execute('INSERT INTO ' . _DB_PREFIX_ . $module->config_name . '_products (product_id,fyndiq_id) VALUES (' . FmHelpers::db_escape(
+            $product_id
+        ) . ',' . FmHelpers::db_escape($fyndiq_id) . ')');
+        return $ret;
+    }
+    /**
      * install table to database
      *
      * @return bool
@@ -13,22 +27,17 @@ class FmProductExport {
             'create table if not exists '._DB_PREFIX_.$module->config_name.'_products (
             id int(20) unsigned primary key,
             product_id int(10) unsigned,
-            fyndiq_id int(20) unsigned)
+            fyndiq_id int(20) unsigned,
+            exported_amount int(20) unsigned)
         ');
         return $ret;
     }
 
-    /**
-     * Create a row in table for mapping the products together.
-     *
-     * @param $product_id
-     * @param $fyndiq_id
-     */
-    public static function create($product_id, $fyndiq_id) {
+    public static function updateStock($product_id, $amount) {
         $module = Module::getInstanceByName('fyndiqmerchant');
-        $ret = (bool)Db::getInstance()->Execute('INSERT INTO ' . _DB_PREFIX_ . $module->config_name . '_products (product_id,fyndiq_id) VALUES (' . FmHelpers::db_escape(
-            $product_id
-        ) . ',' . FmHelpers::db_escape($fyndiq_id) . ')');
+        $ret = (bool)Db::getInstance()->Execute('UPDATE ' . _DB_PREFIX_ . $module->config_name . '_products
+SET exported_amount=' . FmHelpers::db_escape($amount) . '
+WHERE product_id='.FmHelpers::db_escape($product_id).';');
         return $ret;
     }
 
