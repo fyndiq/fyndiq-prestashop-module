9b628510273de88bf88f7d832031ff54bd9498f1
Håkan Nylén <hakan@dun.se>
2014-12-01 10:46:54 +0100

    removed unused codes

--- fyndiqmerchant/backoffice/api.php
+++ fyndiqmerchant/backoffice/api.php
@@ -84,31 +84,6 @@ class FyndiqAPI {
         # try to json decode repsonse data
         $result = json_decode($response['body']);
 
-        // switch (json_last_error()) {
-        //     case JSON_ERROR_NONE:
-        //         echo ' - No errors';
-        //     break;
-        //     case JSON_ERROR_DEPTH:
-        //         echo ' - Maximum stack depth exceeded';
-        //     break;
-        //     case JSON_ERROR_STATE_MISMATCH:
-        //         echo ' - Underflow or the modes mismatch';
-        //     break;
-        //     case JSON_ERROR_CTRL_CHAR:
-        //         echo ' - Unexpected control character found';
-        //     break;
-        //     case JSON_ERROR_SYNTAX:
-        //         echo ' - Syntax error, malformed JSON';
-        //         pd($response['body']);
-        //     break;
-        //     case JSON_ERROR_UTF8:
-        //         echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
-        //     break;
-        //     default:
-        //         echo ' - Unknown error';
-        //     break;
-        // }
-
         # if json_decode failed
         if (json_last_error() != JSON_ERROR_NONE) {
             throw new FyndiqAPIDataInvalid('Error in response data');
