83753eac664aa042092d158b13b47ceb5b85d0a6
Håkan Nylén <hakan@dun.se>
2014-12-16 13:48:21 +0100

    updating support for new product export
    Saving product to db.

--- fyndiqmerchant/backoffice/service.php
+++ fyndiqmerchant/backoffice/service.php
@@ -115,91 +115,20 @@ class FmAjaxService {
     public static function export_products($args) {
         $error = false;
 
+        // Getting all data
         foreach ($args['products'] as $v) {
             $product = $v['product'];
 
-            $product_result = array(
-                'title'=> $product['name'],
-                'description'=> 'asdf8u4389j34g98j34g98',
-                'images'=> array($product['image']),
-                'oldprice'=> '9999',
-                'brand' => 31,
-                'categories' => array("10", "11"),
-                'price'=> $product['price'],
-                'moms_percent'=> '25'
-            );
-
-            $article_result = array();
-
-
-            try {
-                $result = FmHelpers::call_api('POST', 'product/', $product_result);
-                if ($result['status'] != 201) {
-                    $error = true;
-                    self::response_error(
-                        FmMessages::get('unhandled-error-title'),
-                        FmMessages::get('unhandled-error-message')
-                    );
-                }
-                FmProductExport::create($product['id'], $result['data']->id);
-
-                // when posting empty array, it's removed completely from the request, so check for key
-                if (array_key_exists('combinations', $v)) {
-                    $combinations = $v['combinations'];
-
-                    foreach ($combinations as $combination) {
-                        $article_result[] = array(
-                            'num_in_stock'=> '7',
-                            'product' => $result['data']->id,
-                            'property_values' => array("10", "11"),
-                            'item_no'=> '2',
-                            'description'=> 'An test Description'
-                        );
-                    }
-                } else {
-                    $article_result[] = array(
-                        'num_in_stock'=> '10',
-                        'product' => $result['data']->id,
-                        'property_values' => array("10", "11"),
-                        'item_no'=> '99',
-                        'description'=> 'An test Description'
-                    );
-                }
-
-                $result_article = FmHelpers::call_api('POST', 'article/', $article_result);
-                if ($result_article['status'] != 201) {
-                    $error = true;
-                    self::response_error(
-                        FmMessages::get('unhandled-error-title'),
-                        FmMessages::get('unhandled-error-message')
-                    );
-                }
-            } catch (FyndiqAPIBadRequest $e) {
-                $error = true;
-                $message = '';
-                foreach (FyndiqAPI::$error_messages as $error_message) {
-                    $message .= $error_message;
-                }
-                self::response_error(
-                    FmMessages::get('products-bad-params-title'),
-                    $message
-                );
-            } catch (Exception $e) {
-                $error = true;
-                self::response_error(
-                    FmMessages::get('unhandled-error-title'),
-                    $e->getMessage()
-                );
+            if(FmProductExport::productExist($product["id"])) {
+                FmProductExport::updateProduct($product["id"], $product['fyndiq_quantity'], $product['precentage']);
             }
-
-            if ($error) {
-                break;
+            else {
+                FmProductExport::addProduct($product["id"],$product['fyndiq_quantity'], $product['precentage']);
             }
         }
+        FmProductExport::saveFile();
 
-        if (!$error) {
-            self::response();
-        }
+        self::response();
     }
 }
 
--- fyndiqmerchant/fyndiqmerchant.php
+++ fyndiqmerchant/fyndiqmerchant.php
@@ -5,7 +5,8 @@ if (!defined('_PS_VERSION_'))
 
 require_once('messages.php');
 require_once('backoffice/models/config.php');
-require_once('backoffice/api.php');
+require_once('backoffice/includes/api.php');
+require_once('backoffice/includes/fileHandler.php');
 require_once('backoffice/helpers.php');
 require_once('backoffice/controllers.php');
 require_once('backoffice/models/product_export.php');
@@ -19,7 +20,7 @@ class FyndiqMerchant extends Module {
         $this->version = '0.1';
         $this->author = 'Fyndiq AB';
         $this->need_instance = 0;
-        $this->ps_versions_compliancy = array('min' => '1.5.0', 'max' => '1.5.7');
+        $this->ps_versions_compliancy = array('min' => '1.5.0', 'max' => '1.6.7');
 
         parent::__construct();
 
