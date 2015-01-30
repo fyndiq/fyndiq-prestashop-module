0ac0600b8517017444e1d0e744d9b952bb49cb50
Håkan Nylén <hakan@dun.se>
2015-01-30 14:01:11 +0100

    added table for combinations
    for handling the combinations for products.

--- fyndiqmerchant/backoffice/models/product_export.php
+++ fyndiqmerchant/backoffice/models/product_export.php
@@ -1,20 +1,27 @@
 <?php
 
-class FmProductExport {
+class FmProductExport
+{
 
     static function productExist($product_id)
     {
         $module = Module::getInstanceByName('fyndiqmerchant');
-        $sql = "SELECT * FROM "._DB_PREFIX_ . $module->config_name."_products WHERE product_id='".$product_id."' LIMIT 1";
+        $sql = "SELECT * FROM " . _DB_PREFIX_ . $module->config_name . "_products WHERE product_id='" . $product_id . "' LIMIT 1";
         $data = Db::getInstance(_PS_USE_SQL_SLAVE_)->ExecuteS($sql);
+
         return count($data) > 0;
     }
 
     static function addProduct($product_id, $export_qty, $exported_price_percentage)
     {
         $module = Module::getInstanceByName('fyndiqmerchant');
-        $data = array('product_id' => (int)$product_id, 'exported_qty' => (int)$export_qty, 'exported_price_percentage' => (int)$exported_price_percentage);
-        $return = Db::getInstance()->insert($module->config_name."_products",$data);
+        $data = array(
+            'product_id' => (int)$product_id,
+            'exported_qty' => (int)$export_qty,
+            'exported_price_percentage' => (int)$exported_price_percentage
+        );
+        $return = Db::getInstance()->insert($module->config_name . "_products", $data);
+
         return $return;
     }
 
@@ -22,13 +29,98 @@ class FmProductExport {
     {
         $module = Module::getInstanceByName('fyndiqmerchant');
         $data = array('exported_qty' => $export_qty, 'exported_price_percentage' => $exported_price_percentage);
-        return (bool)Db::getInstance()->update($module->config_name."_products",$data, "product_id == '{$product_id}'",1);
+
+        return (bool)Db::getInstance()->update(
+            $module->config_name . "_products",
+            $data,
+            "product_id == '{$product_id}'",
+            1
+        );
     }
 
     public static function deleteProduct($product_id)
     {
         $module = Module::getInstanceByName('fyndiqmerchant');
-        return (bool)Db::getInstance()->delete($module->config_name."_products", "product_id == '{$product_id}'", 1);
+
+        Db::getInstance()->delete($module->config_name . "_products_combos", "product_id == '{$product_id}'");
+
+        return (bool)Db::getInstance()->delete($module->config_name . "_products", "product_id == '{$product_id}'", 1);
+    }
+
+
+    /**
+     * Check if combonation exists
+     *
+     * @param $product_id
+     * @param $combo_id
+     * @return bool
+     */
+    static function comboExist($product_id, $combo_id)
+    {
+        $module = Module::getInstanceByName('fyndiqmerchant');
+        $sql = "SELECT * FROM " . _DB_PREFIX_ . $module->config_name . "_products_combos WHERE product_id='" . $product_id . "' AND combo_id='" . $combo_id . "' LIMIT 1";
+        $data = Db::getInstance(_PS_USE_SQL_SLAVE_)->ExecuteS($sql);
+
+        return count($data) > 0;
+    }
+
+    /**
+     * add a combo for product
+     *
+     * @param integer $product_id
+     * @param integer $combo_id
+     * @param integer $export_qty
+     * @param integer $exported_price_percentage
+     * @return mixed
+     */
+    static function addCombo($product_id, $combo_id, $export_qty, $exported_price_percentage)
+    {
+        $module = Module::getInstanceByName('fyndiqmerchant');
+        $data = array(
+            'product_id' => (int)$product_id,
+            'combo_id' => (int)$combo_id,
+            'exported_qty' => (int)$export_qty,
+            'exported_price_percentage' => (int)$exported_price_percentage
+        );
+        $return = Db::getInstance()->insert($module->config_name . "_products_combos", $data);
+
+        return $return;
+    }
+
+    /**
+     * update combo for product
+     *
+     * @param integer $product_id
+     * @param integer $combo_id
+     * @param integer $export_qty
+     * @param integer $exported_price_percentage
+     * @return bool
+     */
+    public static function updateCombo($product_id, $combo_id, $export_qty, $exported_price_percentage)
+    {
+        $module = Module::getInstanceByName('fyndiqmerchant');
+        $data = array('exported_qty' => $export_qty, 'exported_price_percentage' => $exported_price_percentage);
+
+        return (bool)Db::getInstance()->update(
+            $module->config_name . "_products_combos",
+            $data,
+            "product_id == '{$product_id}' AND combo_id == '{$combo_id}'",
+            1
+        );
+    }
+
+    /**
+     * delete a combo for product
+     *
+     * @param integer $product_id
+     * @param integer $combo_id
+     * @return bool
+     */
+    public static function deleteCombo($product_id, $combo_id)
+    {
+        $module = Module::getInstanceByName('fyndiqmerchant');
+
+        return (bool)Db::getInstance()->delete($module->config_name . "_products_combos", "product_id == '{$product_id}' AND combo_id == '{$combo_id}'", 1);
     }
 
     /**
@@ -36,22 +128,27 @@ class FmProductExport {
      *
      * @return bool
      */
-    public static function install() {
+    public static function install()
+    {
         $module = Module::getInstanceByName('fyndiqmerchant');
         $ret = (bool)Db::getInstance()->Execute(
-            'create table if not exists '._DB_PREFIX_.$module->config_name.'_products (
+            'create table if not exists ' . _DB_PREFIX_ . $module->config_name . '_products (
             id int(20) unsigned primary key AUTO_INCREMENT,
             product_id int(10) unsigned,
             exported_qty int(20) unsigned,
             exported_price_percentage int(20) unsigned)
-        ');
+        '
+        );
         $ret2 = (bool)Db::getInstance()->Execute(
-            'create table if not exists '._DB_PREFIX_.$module->config_name.'_products_combos (
+            'create table if not exists ' . _DB_PREFIX_ . $module->config_name . '_products_combos (
             id int(20) unsigned primary key AUTO_INCREMENT,
+            product_id int(10) unsigned,
             combo_id int(10) unsigned,
             exported_qty int(20) unsigned,
             exported_price_percentage int(20) unsigned)
-        ');
+        '
+        );
+
         return $ret && $ret2;
     }
 
@@ -60,11 +157,13 @@ class FmProductExport {
      *
      * @return bool
      */
-    public static function uninstall() {
+    public static function uninstall()
+    {
         $module = Module::getInstanceByName('fyndiqmerchant');
         $ret = (bool)Db::getInstance()->Execute(
             'drop table ' . _DB_PREFIX_ . $module->config_name . '_products'
         );
+
         return $ret;
     }
 
@@ -74,18 +173,19 @@ class FmProductExport {
         $module = Module::getInstanceByName('fyndiqmerchant');
         $sql = 'SELECT * FROM ' . _DB_PREFIX_ . $module->config_name . '_products';
         $products = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
-        if($products != false) {
+        if ($products != false) {
             $return_array = array();
             foreach ($products as $product) {
 
                 $magarray = FmProduct::get($product["product_id"]);
-
                 $real_array = array();
 
                 $real_array["product-id"] = $product["product_id"];
                 $real_array["article-quantity"] = $product["exported_qty"];
-                $real_array["product-price"] = $magarray["price"]-($magarray["price"]*($product["exported_price_percentage"] / 100));
-                $real_array["product-image-1"] = $product["image"];
+                $real_array["product-price"] = $magarray["price"] - ($magarray["price"] * ($product["exported_price_percentage"] / 100));
+                if (isset($product["image"])) {
+                    $real_array["product-image-1"] = $product["image"];
+                }
                 $real_array["product-title"] = $magarray["name"];
                 $return_array[] = $real_array;
             }
@@ -96,8 +196,7 @@ class FmProductExport {
             foreach ($return_array as $product_array) {
                 $filehandler->appendToFile($product_array);
             }
-        }
-        else {
+        } else {
             return false;
         }
     }
