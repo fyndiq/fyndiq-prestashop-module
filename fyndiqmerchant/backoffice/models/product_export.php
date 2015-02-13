6f5176f98a3fda967b77a545e3e8a9cac8495b7a
Håkan Nylén <hakan@dun.se>
2015-02-13 14:03:08 +0100

    added support for articles and properties

--- fyndiqmerchant/backoffice/models/product_export.php
+++ fyndiqmerchant/backoffice/models/product_export.php
@@ -93,14 +93,45 @@ class FmProductExport
                 $magarray = FmProduct::get($product["product_id"]);
                 $real_array = array();
 
-                $real_array["product-id"] = $product["product_id"];
-                $real_array["article-quantity"] = $product["exported_qty"];
-                $real_array["product-price"] = $magarray["price"] - ($magarray["price"] * ($product["exported_price_percentage"] / 100));
-                if (isset($product["image"])) {
-                    $real_array["product-image-1"] = $product["image"];
+                $real_array = self::getProductData($magarray,$product);
+
+
+                if (count($magarray['combinations']) > 0) {
+                    $first_array = array_shift($magarray['combinations']);
+                    $real_array["article-quantity"] = $first_array["quantity"];
+                    $real_array["product-price"] = $first_array["price"] - ($first_array["price"] * ($product["exported_price_percentage"] / 100));
+                    $real_array["product-oldprice"] = number_format((float)$first_array["price"], 2, '.', '');
+                    $name = "";
+                    $id=1;
+                    foreach($first_array["attributes"] as $attr) {
+                        $name .= $attr["name"] . ": " . $attr["value"];
+                        $real_array["article‑property‑name‑".$id] = $attr["name"];
+                        $real_array["article‑property‑value‑".$id] = $attr["value"];
+                        $id++;
+                    }
+                    $real_array["article-name"] = $name;
+                    $return_array[] = $real_array;
+                    foreach($magarray["combinations"] as $combo) {
+                        $real_array = self::getProductData($magarray,$product);
+                        $real_array["article-quantity"] = $combo["quantity"];
+                        $real_array["article-location"] =
+                        $real_array["product-price"] = $combo["price"] - ($combo["price"] * ($product["exported_price_percentage"] / 100));
+                        $real_array["product-oldprice"] = number_format((float)$combo["price"], 2, '.', '');
+                        $name = "";
+                        $id=1;
+                        foreach($combo["attributes"] as $attr) {
+                            $name .= $attr["name"] . ": " . $attr["value"];
+                            $real_array["article‑property‑name‑".$id] = $attr["name"];
+                            $real_array["article‑property‑value‑".$id] = $attr["value"];
+                            $id++;
+                        }
+                        $real_array["article-name"] = $name;
+                        $return_array[] = $real_array;
+                    }
                 }
-                $real_array["product-title"] = $magarray["name"];
-                $return_array[] = $real_array;
+                else {
+                    $return_array[] = $real_array;
+                }
             }
             $first_array = array_values($return_array)[0];
             $key_values = array_keys($first_array);
@@ -112,5 +143,19 @@ class FmProductExport
         } else {
             return false;
         }
+    }
+    private static function getProductData($magarray, $product) {
+        $real_array = array();
+        $real_array["product-id"] = $product["product_id"];
+        $real_array["article-quantity"] = 0;
+        $real_array["product-price"] = $magarray["price"] - ($magarray["price"] * ($product["exported_price_percentage"] / 100));
+        $real_array["product-oldprice"] = number_format((float)$magarray["price"], 2, '.', '');
+        $real_array["product-brand"] = "test";
+        if (isset($product["image"])) {
+            $real_array["product-image-1-url"] = $product["image"];
+        }
+        $real_array["product-title"] = $magarray["name"];
+        $real_array["product-vat-percent"] = "25";
+        return $real_array;
     }
 }
