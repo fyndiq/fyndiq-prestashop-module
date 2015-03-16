03f92854ab85c2eacfd4f64767c1b5416a5cfc14
Håkan Nylén <hakan@dun.se>
2015-03-16 08:49:32 +0100

    corrected some more

--- fyndiqmerchant/backoffice/filePage.php
+++ fyndiqmerchant/backoffice/filePage.php
@@ -14,7 +14,7 @@ require_once('./models/product.php');
 require_once('./includes/fileHandler.php');
 class FilePageController
 {
-    const FILE_PATH = "/files/feed.csv";
+    const FILE_PATH = '/files/feed.csv';
 
     public function __construct()
     {
@@ -24,7 +24,7 @@ class FilePageController
     public function getFile()
     {
         $result = "";
-        if ($this->username != "" && $this->api_token != "") {
+        if (!empty($this->username) && !empty($this->api_token)) {
 
             //Check if feed file exist and if it is too old
             $fileHandler = new FmFileHandler(_PS_ROOT_DIR_);
@@ -32,7 +32,7 @@ class FilePageController
 
             if ($fileexists) {
                 // If feed last modified date is older than 1 hour, create a new one
-                if (filemtime(_PS_ROOT_DIR_.FILE_PATH) < strtotime('-1 hour', time())) {
+                if (filemtime(_PS_ROOT_DIR_.self::FILE_PATH) < strtotime('-1 hour', time())) {
                     FmProductExport::saveFile(_PS_ROOT_DIR_);
                 }
             } else {
