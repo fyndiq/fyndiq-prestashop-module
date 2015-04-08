9f026b5ef37697b68f7252b3195b5d221b150e42
aquilax <aquilax@gmail.com>
2015-04-08 09:55:44 +0200

    Notifications service

--- fyndiqmerchant/backoffice/notification_service.php
+++ fyndiqmerchant/backoffice/notification_service.php
@@ -3,26 +3,49 @@
 This file handles incoming requests from the automated notification system at Fyndiq.
 */
 
-# import PrestaShop config, to enable use of PrestaShop classes, like Configuration
-$configPath = dirname(dirname(dirname(dirname($_SERVER['SCRIPT_FILENAME'])))).'/config/config.inc.php';
-if (file_exists($configPath)) {
-    require_once($configPath);
-} else {
-    exit;
-}
-
+require_once('./service_init.php');
 require_once('./helpers.php');
-
+require_once('./models/config.php');
+require_once('./models/order.php');
 
 class FmNotificationService {
-    public static function main() {
-        pd($_GET);
-        pd($_POST);
-        $path_info = explode('/', $_SERVER['PATH_INFO']);
-        $shop_id = $path_info[1];
-        $passkey = $path_info[2];
+
+    public static function main($params) {
+        $event = isset($params['event']) ? $params['event'] : false;
+        if ($event) {
+            if (method_exists('FmNotificationService', $event)) {
+                return self::$event($params);
+            }
+        }
+        header('HTTP/1.0 400 Bad Request');
+    }
+
+    /**
+     * Processes new order notifications
+     *
+     * @param $params
+     * @return bool
+     * @throws PrestaShopException
+     */
+    private static function order_created($params) {
+        $orderId = isset($params['order_id']) && is_numeric($params['order_id']) ? $params['order_id'] : 0;
+        if ($orderId) {
+            $url = 'orders/' . $orderId . '/';
+            try {
+                $ret = FmHelpers::call_api('GET', $url);
+                $order = $ret['data'];
+                if (!FmOrder::orderExists($order->id)) {
+                    FmOrder::create($order);
+                }
+            } catch (Exception $e) {
+                header('HTTP/1.0 500 Interna
+                l Server Error');
+            }
+            return true;
+        }
+        header('HTTP/1.0 400 Bad Request');
     }
 }
 
 
-FmNotificationService::main();
+FmNotificationService::main($_GET);
