d6c471d97f704fa7e77d5ee80a2cce0d1246e7a0
aquilax <aquilax@gmail.com>
2015-04-23 11:31:37 +0200

    Added comments and logic to product_info class

--- fyndiqmerchant/backoffice/models/product_info.php
+++ fyndiqmerchant/backoffice/models/product_info.php
@@ -13,6 +13,11 @@ class FmProductInfo
         $this->dbConn = DB::getInstance();
     }
 
+    /**
+     * Gets all products' info
+     *
+     * @return bool
+     */
     public function getAll()
     {
         $nextPagePath = 'product_info/';
@@ -30,25 +35,51 @@ class FmProductInfo
         return $result;
     }
 
+    /**
+     * Get product single page products' info
+     *
+     * @param string $path
+     * @return mixed
+     */
     public function getPageData($path)
     {
         $ret = FmHelpers::callApi('GET', $path);
         return $ret['data'];
     }
 
+    /**
+     * Gets the usleep interval
+     *
+     * @param float $start starting time
+     * @param float $stop stopping time
+     * @param int $max $maximum sleeping time in nanoseconds
+     * @return int mixed nanoseconds to sleep before next request
+     */
     private function getUSleepInterval($start, $stop, $max)
     {
-        return min($max, $max - intval((microtime(true) - $start) * self::NS_IN_SEC));
+        return min($max, $max - intval(($stop - $start) * self::NS_IN_SEC));
     }
 
+    /**
+     * Gets page path from pagination URL
+     *
+     * @param string $url
+     * @return string
+     */
     private function getPath($url)
     {
         if (empty($url)) {
-            return false;
+            return '';
         }
         return implode('/', array_slice(explode('/', $url), 5));
     }
 
+    /**
+     * Update product status
+     *
+     * @param mixed $data
+     * @return bool
+     */
     private function updateProducts($data) {
         $result = true;
         foreach ($data as $statusRow) {
