3b004566a7fdb0c02bd218aa316659f6f6385c13
aquilax <aquilax@gmail.com>
2015-10-30 07:45:31 +0100

    Add processes

--- src/override/controllers/admin/AdminProductsController.php
+++ src/override/controllers/admin/AdminProductsController.php
@@ -17,5 +17,25 @@ class AdminProductsController extends AdminProductsControllerCore
             'icon' => 'icon-minus',
             'confirm' => $this->l('Remove from Fyndiq?')
         );
+        $this->actions_available = array_merge($this->actions_available, array('export_to_fyndiq', 'remove_from_fyndiq'));
     }
+
+    protected function processBulkExportToFyndiq(){
+        if (is_array($this->boxes) && !empty($this->boxes)){
+            error_log('EXPORT: ' . json_encode($this->boxes));
+            foreach ($this->boxes as $product_id){
+
+            }
+        }
+    }
+
+    protected function processRemoveFromFyndiq(){
+        if (is_array($this->boxes) && !empty($this->boxes)){
+            error_log('REMOVE: ' . json_encode($this->boxes));
+            foreach ($this->boxes as $product_id){
+
+            }
+        }
+    }
+
 }
