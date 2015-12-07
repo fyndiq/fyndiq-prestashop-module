1726789d9d26c18906c605d1dc6b1b075f05cd57
aquilax <aquilax@gmail.com>
2015-12-07 08:00:09 +0100

    Removed debugging

--- src/backoffice/FmOrderFetch.php
+++ src/backoffice/FmOrderFetch.php
@@ -17,7 +17,7 @@ class FmOrderFetch extends FyndiqPaginatedFetch
         $url = 'orders/';
         $date = $this->fmConfig->get('import_date', $this->storeId);
         if (!empty($date)) {
-            //$url .= '?min_date=' . urlencode($date);
+            $url .= '?min_date=' . urlencode($date);
         }
         return $url;
     }
@@ -33,44 +33,6 @@ class FmOrderFetch extends FyndiqPaginatedFetch
         $idOrderState = $this->fmConfig->get('import_state', $this->storeId);
         $taxAddressType = $this->fmPrestashop->getTaxAddressType();
         $skuTypeId = intval($this->fmConfig->get('sku_type_id', $this->storeId));
-        $torder = <<<TXT
-{
-    "id": 17,
-    "created": "2015-12-04T07:56:05",
-    "delivery_phone": "5551234567890",
-    "delivery_company": "",
-    "delivery_firstname": "John",
-    "delivery_lastname": "Smith",
-    "delivery_address": "Main Street 1",
-    "delivery_co": "",
-    "delivery_postalcode": "12345",
-    "delivery_city": "Springfield",
-    "delivery_country": "Sweden",
-    "delivery_country_code": "SE",
-    "order_rows": [{
-        "sku": "poruduct1",
-        "vat_percent": "25.00",
-        "unit_price_currency": "SEK",
-        "unit_price_amount": "1.00",
-        "quantity": 1,
-        "productId": "9",
-        "combinationId": 0
-    }, {
-        "sku": "product3",
-        "vat_percent": "25.00",
-        "unit_price_currency": "SEK",
-        "unit_price_amount": "2.00",
-        "quantity": 1,
-        "productId": "9",
-        "combinationId": 0
-    }
-    ],
-    "delivery_note": "https:\/\/cdn.fyndiq.se\/upload\/test_order\/DeliveryNote.pdf"
-}
-TXT;
-        $order = json_decode($torder);
-        return $this->fmOrder->create($order, $idOrderState, $taxAddressType, $skuTypeId);
-
         $errors = array();
         foreach ($data as $order) {
             if (!$this->fmOrder->orderExists($order->id)) {
