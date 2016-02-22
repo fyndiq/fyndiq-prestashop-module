b5d25711ec6b1126f1c84663dfc4cd2df7692014
Md Rakib Miah <rakibuiu@yahoo.com>
2016-02-22 14:44:37 +0100

    integrated auto_updating module

---
+++ src/upgrade/Upgrade-2.0.0.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined('_PS_VERSION_'))
+    exit;
+
+/**
+ * Migration process to upgrade new module version 2.0.0
+ * @param  [Object] $module  [description]
+ * @return [boolean]         [description]
+ */
+function upgrade_module_2_0_0($module)
+{
+    $tableName = $module->getFmPrestashop()->getTableName(FmUtils::MODULE_NAME, '_orders', true);
+    $sql = 'ALTER TABLE ' . $tableName . '
+           ADD COLUMN status INT(10) DEFAULT 1,
+           ADD COLUMN body TEXT DEFAULT null,
+           ADD COLUMN created timestamp DEFAULT CURRENT_TIMESTAMP';
+   return $module->getFmPrestashop()->dbGetInstance()->Execute($sql, false);
+}
+
