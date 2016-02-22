ea9817fb2c643d43efd65dab373bd61f3f5563da
Md Rakib Miah <rakibuiu@yahoo.com>
2016-02-22 16:37:10 +0100

    oatched

--- src/upgrade/Upgrade-2.0.0.php
+++ src/upgrade/Upgrade-2.0.0.php
@@ -11,7 +11,6 @@ if (!defined('_PS_VERSION_')) {
  */
 function upgrade_module_2_0_0($module)
 {
-
     $res = true;
     $tableName = $module->getFmPrestashop()->getTableName(FmUtils::MODULE_NAME, '_orders', true);
     $sql = 'ALTER TABLE ' . $tableName . '
@@ -22,7 +21,7 @@ function upgrade_module_2_0_0($module)
 
     $tableName = $module->getFmPrestashop()->getTableName(FmUtils::MODULE_NAME, '_products', true);
     $sql = 'ALTER TABLE ' . $tableName . ' ADD COLUMN store_id int(10) unsigned DEFAULT 1 AFTER id';
-    $res &= $module->getFmPrestashop()->dbGetInstance()->ExecuteS($sql);
+    $res &= $module->getFmPrestashop()->dbGetInstance()->Execute($sql);
 
     return (bool) $res;
 }
