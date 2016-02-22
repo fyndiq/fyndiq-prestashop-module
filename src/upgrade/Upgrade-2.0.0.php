d9b964f23e9592845e243d9d0e8eb59f6080de28
Md Rakib Miah <rakibuiu@yahoo.com>
2016-02-22 16:04:46 +0100

    added product patch

--- src/upgrade/Upgrade-2.0.0.php
+++ src/upgrade/Upgrade-2.0.0.php
@@ -11,10 +11,17 @@ if (!defined('_PS_VERSION_')) {
  */
 function upgrade_module_2_0_0($module)
 {
+    $res = true;
     $tableName = $module->getFmPrestashop()->getTableName(FmUtils::MODULE_NAME, '_orders', true);
     $sql = 'ALTER TABLE ' . $tableName . '
            ADD COLUMN status INT(10) DEFAULT 1,
            ADD COLUMN body TEXT DEFAULT null,
            ADD COLUMN created timestamp DEFAULT CURRENT_TIMESTAMP';
-    return $module->getFmPrestashop()->dbGetInstance()->Execute($sql, false);
+    $res &= $module->getFmPrestashop()->dbGetInstance()->Execute($sql, false);
+
+    $tableName = $module->getFmPrestashop()->getTableName(FmUtils::MODULE_NAME, '_products', true);
+    $sql = 'ALTER TABLE ' . $tableName . ' ADD COLUMN store_id int(10) unsigned DEFAULT 1 AFTER id';
+    $res &= $module->getFmPrestashop()->dbGetInstance()->ExecuteS($sql);
+
+    return $res;
 }
