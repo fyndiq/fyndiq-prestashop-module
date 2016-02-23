e9b3b6d0e2784c7d764a32b0889c94d676e029f7
Md Rakib Miah <rakibuiu@yahoo.com>
2016-02-23 10:56:14 +0100

    upgrade script

--- src/upgrade/Upgrade-2.0.0.php
+++ src/upgrade/Upgrade-2.0.0.php
@@ -5,38 +5,12 @@ if (!defined('_PS_VERSION_')) {
 }
 
 /**
+ * There is no upgrade in this version
  * Migration process to upgrade new module version 2.0.0
  * @param  [Object] $module  [description]
  * @return [boolean]         [description]
  */
 function upgrade_module_2_0_0($module)
 {
-
-   $res = true;
-    $tableName = $module->getFmPrestashop()->getTableName(FmUtils::MODULE_NAME, '_orders', true);
-    $sql = 'ALTER TABLE ' . $tableName . '
-           DROP COLUMN status,
-           DROP COLUMN body,
-           DROP COLUMN created';
-    $module->getFmPrestashop()->dbGetInstance()->Execute($sql, false);
-
-    $tableName = $module->getFmPrestashop()->getTableName(FmUtils::MODULE_NAME, '_products', true);
-    $sql = 'ALTER TABLE ' . $tableName . ' DROP COLUMN store_id';
-    $module->getFmPrestashop()->dbGetInstance()->Execute($sql);
-    //exit($res."_");
-
-    $res = true;
-    $tableName = $module->getFmPrestashop()->getTableName(FmUtils::MODULE_NAME, '_orders', true);
-    $sql = 'ALTER TABLE ' . $tableName . '
-           ADD COLUMN status INT(10) DEFAULT 1,
-           ADD COLUMN body TEXT DEFAULT null,
-           ADD COLUMN created timestamp DEFAULT CURRENT_TIMESTAMP';
-    $res &= $module->getFmPrestashop()->dbGetInstance()->Execute($sql, false);
-
-    $tableName = $module->getFmPrestashop()->getTableName(FmUtils::MODULE_NAME, '_products', true);
-    $sql = 'ALTER TABLE ' . $tableName . ' ADD COLUMN store_id int(10) unsigned DEFAULT 1 AFTER id';
-    $res &= $module->getFmPrestashop()->dbGetInstance()->Execute($sql);
-
-
-    return (bool) $res;
+  return true;
 }
