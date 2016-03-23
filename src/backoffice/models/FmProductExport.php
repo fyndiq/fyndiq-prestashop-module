b50dad293647ea4e9c464624f98f0db1c51d8718
Håkan Nylén <hakan@dun.se>
2016-03-23 14:46:06 +0100

    fixed fyndiqPrice for combinations

--- src/backoffice/models/FmProductExport.php
+++ src/backoffice/models/FmProductExport.php
@@ -533,7 +533,7 @@ class FmProductExport extends FmModel
                     FyndiqUtils::debug('minimal_quantity > 1 SKIPPING ARTICLE', $combination['minimal_quantity']);
                     continue;
                 }
-                $fyndiqPrice = FyndiqUtils::getFyndiqPrice($combination['price'], $fmProduct['exported_price_percentage']);
+                $fyndiqPrice = FyndiqUtils::getFyndiqPrice($combination['price'], $settings[FmFormSetting::SETTINGS_PERCENTAGE_DISCOUNT]);
 
                 $article = array(
                     FyndiqFeedWriter::ID => $combination['id'],
