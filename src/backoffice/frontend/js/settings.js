dd572d7b248703e2d57e40223001aafe9460f3ce
Md Rakib Miah <rakibuiu@yahoo.com>
2016-03-30 14:20:32 +0200

    resolved switch to dropdown for version changes

--- src/backoffice/frontend/js/settings.js
+++ src/backoffice/frontend/js/settings.js
@@ -1,6 +1,6 @@
 
 $(function() {
-    $('input[name="is_active_cron_task"]').change(function(){
+    $('select[name="is_active_cron_task"]').change(function(){
         if($(this).val() == '1'){
             $('#fm_interval').removeAttr('disabled');
         }else{
