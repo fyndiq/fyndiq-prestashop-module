9d070a7fda9e8d419552d92596bec4394f30125a
Håkan Nylén <hakan@dun.se>
2016-04-05 09:57:02 +0200

    fixed config file

--- .php_cs.php
+++ .php_cs.php
@@ -2,9 +2,8 @@
 
 $finder = Symfony\CS\Finder\DefaultFinder::create()
     ->exclude('backoffice/includes')
-    ->in(__DIR__.'/src')
-;
+    ->in(__DIR__.'/src');
+
 return Symfony\CS\Config\Config::create()
     ->level(Symfony\CS\FixerInterface::PSR2_LEVEL)
-    ->finder($finder)
-;
+    ->finder($finder);
