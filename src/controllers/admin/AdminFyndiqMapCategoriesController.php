2bcec203ee01655cfaea51a03f2f157bdcbde636
aquilax <aquilax@gmail.com>
2016-04-20 14:40:21 +0200

    Load view correctly

--- src/controllers/admin/AdminFyndiqMapCategoriesController.php
+++ src/controllers/admin/AdminFyndiqMapCategoriesController.php
@@ -7,4 +7,19 @@ class AdminFyndiqMapCategoriesController extends ModuleAdminController {
         parent::__construct();
         error_log('KARAMBA');
     }
+
+    public function initContent()
+    {
+        parent::initContent();
+        $this->setTemplate('category_mapping.tpl');
+    }
+
+    public function createTemplate($tpl_name)
+    {
+        $template = _PS_ROOT_DIR_ . '/modules/'.$this->module->name.'/views/templates/admin/'.$tpl_name;
+        if (file_exists($template) ) {
+            return $this->context->smarty->createTemplate($template, $this->context->smarty);
+        }
+        return parent::createTemplate($tpl_name);
+    }
 }
